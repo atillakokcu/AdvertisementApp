bdd47261c9a0ec37c39a73903916d5a04e58468f
atillakokcu <kokcu.ati@gmail.com>
2023-08-08 00:01:27 +0300

    Advertisement Send methodunun yazılması

--- AdvertisementApp.UI/Controllers/AdvertisementController.cs
+++ AdvertisementApp.UI/Controllers/AdvertisementController.cs
@@ -1,4 +1,6 @@
-﻿using AdvertisementApp.UI.Models;
+﻿using AdvertisementApp.Business.Interfaces;
+using AdvertisementApp.Dto;
+using AdvertisementApp.UI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -7,16 +9,32 @@ namespace AdvertisementApp.UI.Controllers
 {
     public class AdvertisementController : Controller
     {
+        private readonly IAppUserService _userService;
+
+        public AdvertisementController(IAppUserService userService)
+        {
+            _userService = userService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [Authorize(Roles ="Member")]
-        public IActionResult Send(int AdvertisementId)
+        public async Task<IActionResult> Send(int AdvertisementId)
         {
+           
             var userId = int.Parse((User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)).Value);
-            return View( new AdvertisementAppUserCreateModel());
+            var userInfo = await _userService.GetByIdAsync<AppUserListDto>(userId);
+            
+            ViewBag.Gender = userInfo.Data.GenderId;
+            return View( new AdvertisementAppUserCreateModel
+            {
+                AdvertismentId = AdvertisementId,
+                AppUserId = userId,
+
+            });
         }
 
     }
